7bc446bec45812691f5cd294a80c9b3b0ede32db
Giulio Sorrentino <gsorre84@gmail.com>
2023-02-15 10:04:46 +0100

    tradotto in spagnolo

--- Barzellette.avalonia/MainWindow.axaml.cs
+++ Barzellette.avalonia/MainWindow.axaml.cs
@@ -209,7 +209,7 @@ namespace Barzellette.avalonia
         }
         private void mnInfo_click(object sender, RoutedEventArgs e)
         {
-            lblbarzelletta.Content = $"Autore: Giulio Sorrentino © 2023\nVersione: 0.1\nUn semplice fortune personale basato su Avalonia e MariaDB\nLicenza: GPL 3.0 o, secondo la tua opinione, qualsiasi versione successiva.";
+            lblbarzelletta.Content = $"Autore: Giulio Sorrentino © 2023\nVersione: 0.2\nUn semplice fortune personale basato su Avalonia e MariaDB\nLicenza: GPL 3.0 o, secondo la tua opinione, qualsiasi versione successiva.";
         }
     }
 }
